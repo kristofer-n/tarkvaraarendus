978554d248a889d1bf43122a0263fdf2c9d5c51a
krist <kristofer.nagaicevs@vikk.ee>
2025-03-28 11:52:35 +0200

    sissejuh ül 5 tehtud

---
+++ harjutus5/Sissejuhatus/2.py
@@ -0,0 +1,35 @@
+import pygame, os
+pygame.init()
+ 
+#värvid
+red = [255, 0, 0]
+green = [0, 255, 0]
+blue = [0, 0, 255]
+pink = [255, 153, 255]
+lGreen = [153, 255, 153]
+lBlue = [153, 204, 255]
+ 
+#ekraani seaded
+screenX = 640
+screenY = 480
+screen=pygame.display.set_mode([screenX,screenY])
+pygame.display.set_caption("Surface")
+screen.fill(lBlue)
+
+#Rect kasutamine
+player = pygame.Rect(0,0, 120, 140)
+pygame.draw.rect(screen, red, player)
+playerImage = pygame.image.load("harjutus5/Sissejuhatus/img/knight.png")
+playerImage = pygame.transform.scale(playerImage, [player.width, player.height])
+
+screen.blit(playerImage,player)
+
+gameover = False
+while not gameover:
+    #mängu sulgemine ristist
+    event = pygame.event.poll()
+    if event.type == pygame.QUIT:
+        break
+
+    pygame.display.flip()
+pygame.quit()
